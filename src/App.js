45aa03379da808444621a5bcacb56e47f9b2dd77
Jake Brown <JakeBrown@jbrownjake.com>
2023-06-15 18:22:22 -0400

    wire frame

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='wrapper'>
+        <h2>25 + 5 Clock</h2>
+        <div className='break-session-length'>
+          <div>
+            <h3 id='break-label'>Break Length</h3>
+            <div>
+              <button disabled={play} onCLick={handleBreakIncrease} id='break-increment'>Increase</button>
+              <strong id='break-length'>{breakLength}</strong>
+              <button disabled={play} onClick={handleBreakDecrease} id='break-decrement'>Decrease</button>
+            </div>
+          </div>
+          <div>
+            <h3 id='session-label'>Session Length</h3>
+            <div>
+              <button disabled={play} onClick={handleSessionIncrease} id='session-increment'>Increase</button>
+              <strong id='session-length'>{sessionLength}</strong>
+              <button disabled={play} onClick={handleSessionDecrease} id='session-decrement'>Decrease</button>
+            </div>
+          </div>
+        </div>
+        <div className='time-wrapper'>
+          <div className='timer'>
+            <h2 id='timer-label'>{title}</h2>
+            <h3 id='time-left'>{timeFormatter()}</h3>
+          </div>
+          <button onClick={handlePlay} id='start-stop'>Start/Stop</button>
+          <button onCLick={handleReset} id='reset'>Reset</button>
+        </div>
+      </div>
+      <audio
+      id='beep'
+      preload='auto'
+      src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+       />
     </div>
   );
 }
